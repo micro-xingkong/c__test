e76037cfa686c7a50ede499078d3d78c2e5923bd
希然 白 <1220539243@qq.com>
2024-09-28 17:33:57 +0800

    9.28 17

--- c++入门/01结构体.cpp
+++ c++入门/01结构体.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
-using namespace std;
-
-struct Student
-{
-	string name;
-	int age;
-	int score;
-};
-int main() {
-
-
-	struct Student s = { "张三",20,80 };
-	
-	Student* p = &s;
-
-	cout << p->age;
-
-
-}
+//#include<iostream>
+//using namespace std;
+//
+//struct Student
+//{
+//	string name;
+//	int age;
+//	int score;
+//};
+//int main() {
+//
+//
+//	struct Student s = { "张三",20,80 };
+//	
+//	Student* p = &s;
+//
+//	cout << p->age;
+//
+//
+//}
