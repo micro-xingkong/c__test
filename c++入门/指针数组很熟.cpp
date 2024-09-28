65220d22acb4a8700c34b6742d4f4669822e7489
希然 白 <1220539243@qq.com>
2024-09-28 12:35:22 +0800

    24.9.28 12:35

--- c++入门/指针数组很熟.cpp
+++ c++入门/指针数组很熟.cpp
@@ -1,38 +1,40 @@
-#include<iostream>
-using namespace std;
-
-
-void maopao(int * arr,int length) {
-
-	for (int i = 0; i < length-1; i++)
-	{
-		for (int j = 0; j < length-j-1; j++)
-		{
-			
-			if (*arr<*arr)
-			{
-				int t = *arr;
-				*arr = *(arr++);
-				*(arr++) = t;
-			}
-		}
-	}
-
-	
-
-}
-
-
-void main() {
-
-	int a[10] = { 0,5,8,4,6,7,2,9,3,1 };
-
-	int * arr = a;
-	cout<< a[0] <<" " << *arr << " " << *arr  << endl;
-
-	maopao(a, 10);
-	for (int h = 0; h < 10; h++)
-	{
-		cout << a[h] << " ";
-	}
-}
+//#include<iostream>
+//using namespace std;
+//
+////void maopao(int arr[], int length)
+//void maopao(int * arr, int length) {
+//	
+//
+//	for (int i = 0; i < length-1; i++)
+//	{
+//		for (int j = 0; j < length-i-1; j++)
+//		{
+//			
+//			
+//			if (arr[j]>arr[j+1])
+//			{
+//				int t = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = t;
+//			}
+//		}
+//	}
+//
+//	
+//
+//}
+//
+//
+//void main() {
+//
+//	int a[10] = { 0,5,8,4,6,7,2,9,3,1 };
+//
+//	/*int * arr = a;
+//	cout<< a[0] <<" " << *arr << " " << *arr  << endl;*/
+//
+//	maopao(a, 10);
+//	for (int h = 0; h < 10; h++)
+//	{
+//		cout << a[h] << " ";
+//	}
+//}
